ecbdfada2041f88d8a28104da7370c65e8cdb8f1
kkstas <stasiakkamilkrzysztof@gmail.com>
2025-02-16 23:18:12 +0100

    test: delete user vault

---
+++ internal/handlers/vault/delete_one_by_id_test.go
@@ -0,0 +1,65 @@
+package vault_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/kkstas/tr-backend/internal/testutils"
+)
+
+func TestDeleteOneByID(t *testing.T) {
+	t.Parallel()
+
+	t.Run("deletes existing vault", func(t *testing.T) {
+		t.Parallel()
+		serv, db := testutils.NewTestApplication(t)
+		token, user := testutils.CreateUserWithToken(t, db)
+
+		vaultService := testutils.NewTestVaultService(db)
+		err := vaultService.CreateOne(context.Background(), user.ID, "some vault name")
+		testutils.AssertNoError(t, err)
+
+		vaults, err := vaultService.FindAll(context.Background(), user.ID)
+		testutils.AssertNoError(t, err)
+		testutils.AssertEqual(t, len(vaults), 1)
+
+		request := httptest.NewRequest("DELETE", "/vaults/"+vaults[0].ID, nil)
+		request.Header.Set("Authorization", "Bearer "+token)
+		response := httptest.NewRecorder()
+		serv.ServeHTTP(response, request)
+
+		testutils.AssertStatus(t, response.Code, http.StatusNoContent)
+
+		vaults, err = vaultService.FindAll(context.Background(), user.ID)
+		testutils.AssertNoError(t, err)
+		testutils.AssertEqual(t, len(vaults), 0)
+	})
+
+	t.Run("does not delete existing vault if user is not assigned to the vault", func(t *testing.T) {
+		t.Parallel()
+		serv, db := testutils.NewTestApplication(t)
+		_, user := testutils.CreateUserWithToken(t, db)
+
+		vaultService := testutils.NewTestVaultService(db)
+		err := vaultService.CreateOne(context.Background(), user.ID, "some vault name")
+		testutils.AssertNoError(t, err)
+
+		vaults, err := vaultService.FindAll(context.Background(), user.ID)
+		testutils.AssertNoError(t, err)
+		testutils.AssertEqual(t, len(vaults), 1)
+
+		otherUserToken, _ := testutils.CreateUserWithToken(t, db)
+		request := httptest.NewRequest("DELETE", "/vaults/"+vaults[0].ID, nil)
+		request.Header.Set("Authorization", "Bearer "+otherUserToken)
+		response := httptest.NewRecorder()
+		serv.ServeHTTP(response, request)
+
+		testutils.AssertStatus(t, response.Code, http.StatusNotFound)
+
+		vaults, err = vaultService.FindAll(context.Background(), user.ID)
+		testutils.AssertNoError(t, err)
+		testutils.AssertEqual(t, len(vaults), 1)
+	})
+}
